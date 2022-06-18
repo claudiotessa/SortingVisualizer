3292328533b744834ee29fc4fd833ca9fa6a0253
Claudio Tessa <tessa.claudio04@gmail.com>
2022-06-18 18:46:55 +0200

    added other algorithms support

--- js/index.js
+++ js/index.js
@@ -30,20 +30,38 @@ $("#start").click(function () {
         case "bubble":
             values = bubbleSort(values);
             break;
+        case "selection":
+            values = selectionSort(values);
+            break;
+        case "insertion":
+            values = insertionSort(values);
+            break;
         case "quick":
-            quickSort();
+            values = quickSort(values);
+            break;
+        case "counting":
+            values = countingSort(values);
+            break;
+        case "radix":
+            values = radixSort(values);
+            break;
+        case "bucket":
+            values = bucketSort(values);
+            break;
+        case "heap":
+            values = heapSort(values);
+            break;
+        case "shell":
+            values = shellSort(values);
             break;
         default:
             canGo = false;
-            console.log("no algorithm chosen");
+            alert("Select an algorithm");
             break;
     }
 });
 
-const delay = (time) => {
-    return new Promise((resolve) => setTimeout(resolve, time));
-};
-
+const delay = (time) => new Promise((resolve) => setTimeout(resolve, time));
 const generateBars = () => {
     const numBars = $("#num-bars").val();
     const array = [];
@@ -99,9 +117,151 @@ const bubbleSort = async (array) => {
             }
         }
     }
+    endSortAnimation();
     return array;
 };
-const quickSort = () => {};
+const selectionSort = async (array) => {
+    for (let i = 0; i < array.length - 1; i++) {
+        let minPos = i;
+        for (let j = i + 1; j < array.length; j++) {
+            if (!canGo) return;
+            if (array[j] < array[minPos]) {
+                minPos = j;
+            }
+        }
+        if (i !== minPos) {
+            const tmp = array[i];
+            array[i] = array[minPos];
+            array[minPos] = tmp;
 
+            container
+                .children()
+                .eq(i)
+                .text(array[i])
+                .animate({ height: array[i] + "vh" }, sortSpeed)
+                .css({ background: SELECTION_COLORS[0] });
+
+            container
+                .children()
+                .eq(minPos)
+                .text(array[minPos])
+                .animate({ height: array[minPos] + "vh" }, sortSpeed)
+                .css({ background: SELECTION_COLORS[1] });
+
+            await delay(sortSpeed);
+            container
+                .children()
+                .slice(i, minPos + 1)
+                .css({ background: PRIMARY_COLOR });
+        }
+    }
+    endSortAnimation();
+    return array;
+};
+const insertionSort = async (array) => {
+    for (let i = 1; i < array.length; i++) {
+        for (let j = i; j > 0 && array[j] < array[j - 1]; j--) {
+            if (!canGo) return;
+
+            const tmp = array[j];
+            array[j] = array[j - 1];
+            array[j - 1] = tmp;
+
+            container
+                .children()
+                .eq(j)
+                .text(array[j])
+                .animate({ height: array[j] + "vh" }, sortSpeed)
+                .css({ background: SELECTION_COLORS[0] });
+
+            container
+                .children()
+                .eq(j - 1)
+                .text(array[j - 1])
+                .animate({ height: array[j - 1] + "vh" }, sortSpeed)
+                .css({ background: SELECTION_COLORS[1] });
+
+            await delay(sortSpeed);
+            container
+                .children()
+                .slice(j - 1, j + 1)
+                .css({ background: PRIMARY_COLOR });
+        }
+    }
+    endSortAnimation();
+    return array;
+};
+const quickSort = async (array, left = 0, right = array.length - 1) => {
+    const partition = async (arr, left, right) => {
+        var pivot = arr[Math.floor((right + left) / 2)]; //middle element
+        var i = left; //left pointer
+        var j = right; //right pointer
+        while (i <= j) {
+            while (arr[i] < pivot) {
+                i++;
+            }
+            while (arr[j] > pivot) {
+                j--;
+            }
+            if (i <= j) {
+                if (!canGo) return;
+
+                var tmp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = tmp;
+
+                container
+                    .children()
+                    .eq(i)
+                    .text(arr[i])
+                    .animate({ height: arr[i] + "vh" }, sortSpeed)
+                    .css({ background: SELECTION_COLORS[0] });
+
+                container
+                    .children()
+                    .eq(j)
+                    .text(arr[j])
+                    .animate({ height: arr[j] + "vh" }, sortSpeed)
+                    .css({ background: SELECTION_COLORS[1] });
+
+                await delay(sortSpeed);
+                container.children().eq(i).css({ background: PRIMARY_COLOR });
+                container.children().eq(j).css({ background: PRIMARY_COLOR });
+
+                i++;
+                j--;
+            }
+        }
+        return i;
+    };
+
+    let index;
+    if (array.length > 1) {
+        index = await partition(array, left, right); //index returned from partition
+        if (left < index - 1) {
+            //more elements on the left side of the pivot
+            quickSort(array, left, index - 1);
+        }
+        if (index < right) {
+            //more elements on the right side of the pivot
+            quickSort(array, index, right);
+        }
+    }
+    //endSortAnimation();
+    return array;
+};
+const countingSort = async (array) => {};
+const radixSort = async (array) => {};
+const bucketSort = async (array) => {};
+const heapSort = async (array) => {};
+const shellSort = async (array) => {};
+
+const endSortAnimation = async () => {
+    for (let i = 0; i < container.children().length; i++) {
+        container.children().eq(i).css({ background: "#00ff00" });
+        await delay(50);
+    }
+    container.children().css({ background: PRIMARY_COLOR });
+};
 values = generateBars();
 renderBars(values);
